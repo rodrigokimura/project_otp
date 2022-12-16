965053876c01880e9f6c58eaf480f9554cac2911
Rodrigo Eiti Kimura <rodrigo.kimura@catskillet.com>
2022-12-16 14:34:51 -0300

    Add CLI arg

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 export PYTHONPATH := 'scr'
 export PIPENV_VERBOSITY := -1
 
+# If the first argument is "cli"...
+ifeq (run,$(firstword $(MAKECMDGOALS)))
+  # use the rest as arguments for "cli"
+  CLI_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # ...and turn them into do-nothing targets
+  $(eval $(CLI_ARGS):;@:)
+endif
+
 run:
-	@pipenv run python src/script.py
+	@pipenv run python src/script.py $(CLI_ARGS)
 
 install:
 	@pipenv install --dev --skip-lock
--- src/script.py
+++ src/script.py
@@ -1,14 +1,26 @@
 import os
+import sys
+from datetime import datetime, timedelta
 
 import pyotp
 import pyperclip
+from dotenv import load_dotenv
 
 
-def main():
-    otp = pyotp.TOTP(os.getenv("OTP_SECRET")).now()
+def main(copy=False):
+    try:
+        seconds = int(sys.argv[1])
+    except (ValueError, IndexError):
+        seconds = 0
+
+    load_dotenv()
+    otp = pyotp.TOTP(os.getenv("OTP_SECRET")).at(
+        datetime.now() + timedelta(seconds=seconds)
+    )
 
     print(otp)
-    pyperclip.copy(otp)
+    if copy:
+        pyperclip.copy(otp)
 
 
 if __name__ == "__main__":
